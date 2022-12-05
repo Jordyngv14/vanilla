d714dd4dbe16e88dd0fa78040b3816e8229511f2
Jordyngv14 <jordyngv14@gmail.com>
2022-12-05 00:56:36 +0000

    Todo de las funciones

--- src/app.js
+++ src/app.js
@@ -18,3 +18,48 @@ console.log(arrowFunction());
 let arrowFunction1 = a => {
   console.log(a);
 };
+arrowFunction1(50);
+
+let arrowFunction2 = numero => {
+  return numero + 50;
+};
+console.log(arrowFunction2(25));
+
+let arrowFunction3 = (numero, agregar) => {
+  return numero + agregar;
+};
+console.log(arrowFunction3(20, 20));
+
+let arrowFunction4 = (numero, agregar = 0) => {
+  return numero + agregar;
+};
+console.log(arrowFunction4(20));
+
+//Declaration function
+function SumarNumeros() {
+  return [1, 2, 3, 4];
+}
+console.log(SumarNumeros());
+
+function PruebaVarLet() {
+  var varafuera = "var afuera";
+  let letafuera = "let afuera";
+
+  {
+    var varadentro = "var adentro";
+    let letadentro = "let adentro";
+  }
+
+  console.log(varafuera);
+  console.log(letafuera);
+  console.log(varadentro);
+}
+
+PruebaVarLet();
+
+//Expression function
+
+let multiplicar = function(num1, num2) {
+  return num1 * num2;
+};
+console.log(multiplicar(5, 5));
